b46d520ab6513dd7f79dd6c1337ffe763da1b5a2
kokkoniemi <mikko@kokkoniemi.com>
2020-05-01 11:32:03 +0300

    added a json-field for record's alternative urls

--- models/record.js
+++ models/record.js
@@ -9,8 +9,31 @@ module.exports = (sequelize, DataTypes) => {
     abstract: DataTypes.TEXT,
     databases: DataTypes.JSON,
   }, {});
-  Record.associate = function(models) {
+
+  Record.associate = function (models) {
     // associations can be defined here
   };
+
+  /**
+   * Fetches all records that have matching search_url either in url-field or
+   * as an entry in json-array in alternateUrls
+   */
+  Record.getAllByUrl = async function (search_url) {
+    const recordInstances = await sequelize.query(
+      `
+      SELECT Records.* FROM Records WHERE url LIKE ? 
+      UNION
+      SELECT Records.*
+        FROM Records, json_each(Records.alternateUrls)
+      WHERE json_valid(Records.alternateUrls)
+        AND json_each.value LIKE ?
+      `,
+      {
+        replacements: [search_url, search_url],
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+    return recordInstances;
+  }
   return Record;
 };
