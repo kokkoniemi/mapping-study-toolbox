f241d04a77e0df4bb85eb8204baee960d51fdb57
kokkoniemi <mikko@kokkoniemi.com>
2020-05-07 21:25:26 +0300

    improved api pagination

--- routes/api.js
+++ routes/api.js
@@ -11,35 +11,37 @@ router.get("/records", function (req, res) {
     if (status !== undefined) {
         where.status = status === "null" ? null : status;
     }
-    return db.Record.findAll({
-        offset,
-        limit,
-        where,
-        include: 'Publication'
-    }).then((records) => res.send(records))
-    .catch((err) => res.send(err));
+    return db.Record.count({ where }).then(count => {
+        db.Record.findAll({
+            offset,
+            limit,
+            where,
+            include: 'Publication'
+        }).then((records) => res.send({ count, records }))
+    })
+        .catch((err) => res.send(err));
 });
 
 router.get("/records/:id", function (req, res) {
     const id = parseInt(req.params.id);
     return db.Record.findByPk(id)
-    .then((record) => res.send(record))
-    .catch((err) => res.send(err));
+        .then((record) => res.send(record))
+        .catch((err) => res.send(err));
 });
 
 // only enable updating the status of the record
 router.put("/records/:id", function (req, res) {
     const id = parseInt(req.params.id);
     return db.Record.findByPk(id)
-    .then((record) => {
-        const { status, editedBy } = req.body;
-        if (![null, "uncertain", "excluded", "included"].includes(status)) {
-            throw new Error("Illegal value for 'status'");
-        }
-        return record.update({ status, editedBy })
-        .then(() => res.send(record))
-        .catch((err) => res.status(400).send(err));
-    });
+        .then((record) => {
+            const { status, editedBy } = req.body;
+            if (![null, "uncertain", "excluded", "included"].includes(status)) {
+                throw new Error("Illegal value for 'status'");
+            }
+            return record.update({ status, editedBy })
+                .then(() => res.send(record))
+                .catch((err) => res.status(400).send(err));
+        });
 });
 
 module.exports = router;
